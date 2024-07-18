b3c1745a521ea8aefae27f5747fd622699f8eb3a
Konstantin <scub111g@gmail.com>
2024-07-18 20:16:53 +0300

    fix: cleaning

--- contracts/SQRpProRata.sol
+++ contracts/SQRpProRata.sol
@@ -11,7 +11,7 @@ import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol
 import {IContractInfo} from "./IContractInfo.sol";
 import {IDepositRefund} from "./IDepositRefund.sol";
 
-import "hardhat/console.sol";
+// import "hardhat/console.sol";
 
 contract SQRpProRata is
   OwnableUpgradeable,
